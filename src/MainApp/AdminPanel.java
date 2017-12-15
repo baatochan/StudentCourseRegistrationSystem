385346f73653bd7cebcf2b2be7adf34842765b74
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2017-12-15 22:24:55 +0100

    Add templete for panels

--- src/MainApp/AdminPanel.java
+++ src/MainApp/AdminPanel.java
@@ -1,4 +1,14 @@
 package MainApp;
 
+import javafx.stage.Stage;
+
 public class AdminPanel extends Panel {
+	
+	// wywietlenie okna i ukladu elementow w oknie
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+	
+	}
+	
+	// funkcje odpawiadajace na kliknecia na obiekty w oknie
 }
--- src/MainApp/Panel.java
+++ src/MainApp/Panel.java
@@ -1,4 +1,7 @@
 package MainApp;
 
+import javafx.stage.Stage;
+
 public class Panel extends Main { // nw czy na pewno Maina czy nie lepiej po prostu Application
+	//nw czy cokolwiek tu bedzie, czy w ogole ta klasa pozostanie
 }
