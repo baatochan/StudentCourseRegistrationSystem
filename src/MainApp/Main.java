4d58a03f9cc7daa937298faef7a9d9383801529e
Paweł <pako-96@o2.pl>
2017-12-17 16:39:09 +0100

    some changes

--- src/MainApp/Main.java
+++ src/MainApp/Main.java
@@ -35,7 +35,8 @@ public class Main extends Application {
 	
 	
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch(args);
     }
 }
--- src/MainApp/MainController.java
+++ src/MainApp/MainController.java
@@ -1,10 +1,15 @@
 package MainApp;
 
 import javafx.event.ActionEvent;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class MainController {
-	public Label helloWorld;
+
+
+    public Label helloWorld, checkLogin;
+	public PasswordField passField;
+	public TextField loginField;
+	public Button btn;
 	
 	public void sayHelloWorld(ActionEvent actionEvent) {
 		if (helloWorld.getText().equals("")) {
@@ -16,6 +21,9 @@ public class MainController {
 
 	public void loginToProgram(ActionEvent actionEvent)
     {
+        String napis;
+        napis = loginField.getText() + passField.getText();
+        checkLogin.setText(napis);
 
 
     }
