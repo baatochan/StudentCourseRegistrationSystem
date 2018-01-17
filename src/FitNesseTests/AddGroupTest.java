94f9d237065060713e6927bd0a9755e6c78bcf27
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2018-01-17 21:30:23 +0100

    Add fix to fitnesse addGroupTest to mantain consistensy in tests

--- src/FitNesseTests/AddGroupTest.java
+++ src/FitNesseTests/AddGroupTest.java
@@ -13,8 +13,8 @@ public class AddGroupTest extends ColumnFixture {
 		int numberBefore = numberOfGroups();
 
 		try {
-			SetUp.panel.addGroupToDB(data[0], data[1], data[2], data[3], data[4], data[5]);
 			lastCourse = Main.findCourse(data[0]);
+			SetUp.panel.addGroupToDB(data[0], data[1], data[2], data[3], data[4], data[5]);
 		} catch (Exception e) {
 			output = "Wyjatek: " + e.getMessage();
 		}
